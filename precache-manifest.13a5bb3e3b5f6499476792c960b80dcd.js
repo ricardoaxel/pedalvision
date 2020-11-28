459dae0f43c63794b1521a808b7db299e7041684
Ashel <elchetumal@gmail.com>
2020-11-28 12:53:55 -0600

    Updates

---
+++ precache-manifest.13a5bb3e3b5f6499476792c960b80dcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2114a7e7359a72004e1f1b544506977f",
+    "url": "/pedalvision/index.html"
+  },
+  {
+    "revision": "a891c4a3f7ff99894fa0",
+    "url": "/pedalvision/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ae4affafe17eb502ad91",
+    "url": "/pedalvision/static/js/2.02f0a939.chunk.js"
+  },
+  {
+    "revision": "abe08a1506f951fe89cb6492063e650e",
+    "url": "/pedalvision/static/js/2.02f0a939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a891c4a3f7ff99894fa0",
+    "url": "/pedalvision/static/js/main.6e6e9a0b.chunk.js"
+  },
+  {
+    "revision": "c98e6af03482dd75b0e4",
+    "url": "/pedalvision/static/js/runtime-main.86b59119.js"
+  }
+]);
